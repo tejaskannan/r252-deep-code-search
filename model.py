ebc9c71c5cde0b0edcd1d73eaac292ca3a47e35c
Tejas Kannan <tejas.kannan286@gmail.com>
2019-02-07 11:26:33 +0000

    Implement api embedding and maxpooling layers

--- model.py
+++ model.py
@@ -19,7 +19,10 @@ class Model:
         self.max_vocab_size = 50000
         self.seq_length = 50
         self.rnn_units = 16
+        self.dense_units = 16
         self.batch_size = 128
+
+        self.scope = "deep-cs"
 
         self.method_names = self._load_data_file(train_dir + "/" + METHOD_NAME_FILE_NAME)
         self.method_api_calls = self._load_data_file(train_dir + "/" + METHOD_API_FILE_NAME)
@@ -44,10 +47,13 @@ class Model:
         self._sess = tf.Session(graph=tf.Graph())
 
         with self._sess.graph.as_default():
-            self.name_placeholder = tf.placeholder(dtype=tf.int32, shape=[None, None], name='name')
-            self.api_placeholder = tf.placeholder(dtype=tf.int32, shape=[None, None], name='api')
-            self.token_placeholder = tf.placeholder(dtype=tf.int32, shape=[None, None], name='token')
-            self.javadoc_placehoder = tf.placeholder(dtype=tf.int32, shape=[None, None], name='javadoc')
+            self.name_placeholder = tf.placeholder(dtype=tf.int32, shape=[None, self.seq_length], name='name')
+            self.api_placeholder = tf.placeholder(dtype=tf.int32, shape=[None, self.seq_length], name='api')
+            self.token_placeholder = tf.placeholder(dtype=tf.int32, shape=[None, self.seq_length], name='token')
+            self.javadoc_pos_placeholder = tf.placeholder(dtype=tf.int32, shape=[None, self.seq_length],
+                                                          name='javadoc-pos')
+            self.javadoc_neg_placeholder = tf.placeholder(dtype=tf.int32, shape=[None, self.seq_length],
+                                                          name='javadoc-neg')
 
             self.optimizer = tf.train.AdamOptimizer(learning_rate=self.step_size)
 
@@ -61,18 +67,25 @@ class Model:
             init_op = tf.variables_initializer(self._sess.graph.get_collection(tf.GraphKeys.GLOBAL_VARIABLES))
             self._sess.run(init_op)
 
+
+            javadoc_tensors_neg = np.copy(self.javadoc_tensors)
+            np.random.shuffle(javadoc_tensors_neg)
+
             for index in range(0, self.data_count, self.batch_size):
                 end = index + self.batch_size
+
                 name_batch = self.name_tensors[index:end]
                 api_batch = self.api_tensors[index:end]
                 token_batch = self.token_tensors[index:end]
-                javadoc_batch = self.javadoc_tensors[index:end]
+                javadoc_pos_batch = self.javadoc_tensors[index:end]
+                javadoc_neg_batch = javadoc_tensors_neg[index:end]
 
                 feed_dict = {
                     self.name_placeholder: name_batch,
                     self.api_placeholder: api_batch,
                     self.token_placeholder: token_batch,
-                    self.javadoc_placehoder: javadoc_batch
+                    self.javadoc_pos_placeholder: javadoc_pos_batch,
+                    self.javadoc_neg_placeholder: javadoc_neg_batch
                 }
 
                 op_result = self._sess.run(self.loss_op, feed_dict=feed_dict)
@@ -83,37 +96,67 @@ class Model:
 
     def _make_model(self):
 
-        # Method Name embedding
-        one_hot_names = tf.one_hot(self.name_placeholder, depth=1, on_value=1.0, off_value=0.0, dtype=tf.float32)
-        name_cell_fw = tf.nn.rnn_cell.LSTMCell(num_units=self.rnn_units)
-        name_cell_bw = tf.nn.rnn_cell.LSTMCell(num_units=self.rnn_units)
-        name_emb, name_state = tf.nn.bidirectional_dynamic_rnn(
-                                        cell_fw=name_cell_fw,
-                                        cell_bw=name_cell_bw,
-                                        inputs=one_hot_names,
+        with tf.variable_scope(self.scope, reuse=tf.AUTO_REUSE):
+            # Method Name embedding
+            name_emb, name_state = self._make_rnn_embedding(self.name_placeholder,
+                                                           name="name-embedding")
+            name_pooled = tf.layers.max_pooling1d(inputs=name_emb[1],
+                                                  pool_size=(self.seq_length,),
+                                                  strides=(1,),
+                                                  name="name-pooling")
+            name_pooled = tf.squeeze(name_pooled, axis=1)
+
+            # API Embedding
+            api_emb, api_state = self._make_rnn_embedding(self.api_placeholder,
+                                                          name="api-embedding")
+            api_pooled = tf.layers.max_pooling1d(inputs=api_emb[1],
+                                                 pool_size=(self.seq_length,),
+                                                 strides=(1,),
+                                                 name="api-pooling")
+            api_pooled = tf.squeeze(api_pooled, axis=1)
+
+            # Fusion Layer
+            code_concat = tf.concat([name_pooled, api_pooled], axis=1, name="code-concat")
+            code_emb = tf.layers.dense(inputs=code_concat, units=self.dense_units,
+                                       activation=tf.nn.tanh, name="fusion")
+
+            # Javadoc Embeddings
+            jd_pos_emb, jd_pos_state = self._make_rnn_embedding(self.javadoc_pos_placeholder,
+                                                                name="jd-embedding")
+            jd_pos_pooled = tf.layers.max_pooling1d(inputs=jd_pos_emb[1],
+                                                    pool_size=(self.seq_length,),
+                                                    strides=(1,),
+                                                    name="jd-pooling")
+            jd_pos_pooled = tf.squeeze(jd_pos_pooled, axis=1)
+
+            jd_neg_emb, jd_neg_state = self._make_rnn_embedding(self.javadoc_neg_placeholder,
+                                                                name="jd-embedding")
+            jd_neg_pooled = tf.layers.max_pooling1d(inputs=jd_neg_emb[1],
+                                                    pool_size=(self.seq_length,),
+                                                    strides=(1,),
+                                                    name="jd-pooling")
+            jd_neg_pooled = tf.squeeze(jd_neg_pooled, axis=1)
+
+            self.loss_op = tf.math.reduce_sum(
+                    tf.constant(self.margin, dtype=tf.float32) - \
+                    tf.losses.cosine_distance(labels=jd_pos_pooled, predictions=code_emb, axis=1) + \
+                    tf.losses.cosine_distance(labels=jd_neg_pooled, predictions=code_emb, axis=1)
+                )
+
+
+    def _make_rnn_embedding(self, placeholder, name):
+        one_hot = tf.one_hot(placeholder, depth=1, on_value=1.0, off_value=0.0, dtype=tf.float32)
+        cell_fw = tf.nn.rnn_cell.LSTMCell(num_units=self.rnn_units,
+                                          activation=tf.nn.tanh)
+        cell_bw = tf.nn.rnn_cell.LSTMCell(num_units=self.rnn_units,
+                                          activation=tf.nn.tanh)
+        emb, state = tf.nn.bidirectional_dynamic_rnn(
+                                        cell_fw=cell_fw,
+                                        cell_bw=cell_bw,
+                                        inputs=one_hot,
                                         dtype=tf.float32,
-                                        scope="name-embedding")
-
-        # Javadoc Embedding
-        one_hot_javadoc = tf.one_hot(self.javadoc_placehoder, depth=1, on_value=1.0, off_value=0.0, dtype=tf.float32)
-        jd_cell_fw = tf.nn.rnn_cell.LSTMCell(num_units=self.rnn_units)
-        jd_cell_bw = tf.nn.rnn_cell.LSTMCell(num_units=self.rnn_units)
-        jd_emb, jd_state = tf.nn.bidirectional_dynamic_rnn(
-                                        cell_fw=jd_cell_fw,
-                                        cell_bw=jd_cell_bw,
-                                        inputs=one_hot_javadoc,
-                                        dtype=tf.float32,
-                                        scope="javadoc-embedding")
-
-        print(name_emb)
-        print(jd_emb)
-
-        self.loss_op = tf.math.reduce_sum(
-                tf.constant(self.margin, dtype=tf.float32) - \
-                tf.losses.cosine_distance(labels=jd_emb, predictions=name_emb, axis=1)
-            )
-
-
+                                        scope=name)
+        return emb, state
 
     def _tensorize_data(self, method_names, method_api_calls, method_tokens, javadoc):
 
